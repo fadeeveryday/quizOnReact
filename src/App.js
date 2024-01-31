47d2a0c4b67bd0a378ad3ddae6b0339e22e552a5
Vladislave <fadeeverynight@gmail.com>
2024-01-31 20:12:33 +0300

    add iterator for questions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import './index.scss';
+
+const questions = [
+  {
+    title: 'React - это ... ?',
+    variants: ['библиотека', 'фреймворк', 'приложение'],
+    correct: 0,
+  },
+  {
+    title: 'Компонент - это ... ',
+    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    correct: 1,
+  },
+  {
+    title: 'Что такое JSX?',
+    variants: [
+      'Это простой HTML',
+      'Это функция',
+      'Это тот же HTML, но с возможностью выполнять JS-код',
+    ],
+    correct: 2,
+  },
+];
+
+function Result() {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>Вы отгадали 3 ответа из 10</h2>
+      <button>Попробовать снова</button>
+    </div>
+  );
+}
+
+function Game({ question, onClickVariant }) {
+  return (
+    <>
+      <div className="progress">
+        <div style={{ width: '20%' }} className="progress__inner"></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {
+          question.variants.map((text, index) => 
+          <li 
+            onClick={() => onClickVariant(index)}
+            key={index}>
+              {text}
+          </li>)
+        }
+      </ul>
+    </>
+  );
+}
 
 function App() {
+  const [step, setStep] = useState(0)
+  const question = questions[step]
+
+  const onClickVariant = (index) => {
+    console.log(index, step)
+    setStep(step + 1)
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Game question={question} onClickVariant={onClickVariant} />
+      {/* <Result /> */}
     </div>
   );
 }
